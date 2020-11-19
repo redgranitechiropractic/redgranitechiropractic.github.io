2216f4c2e831e9366d854b0f02201b2cb237586b
Dane <danegellerup@uwalumni.com>
2020-11-18 19:27:44 -0800

    display today's hours

--- assets/js/today_hours.js
+++ assets/js/today_hours.js
@@ -3,7 +3,12 @@ window.onload = function () {
   var days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
   var hours = ["Closed", "8:00am - 5:00pm", "8:00am - 1:00pm", "8:00am - 5:00pm", "Closed", "	8:00am - 5:00pm", "Closed"]
 
-  var message = days[d.getDay()] + ": Open " + hours[d.getDay()];
+  if (hours[d.getDay()] != "Closed") {
+    var message = days[d.getDay()] + ": Open " + hours[d.getDay()];
+  } else {
+    var message = days[d.getDay()] + ": Closed";
+  }
+
 
   document.getElementById("intro").innerHTML = message;
 
